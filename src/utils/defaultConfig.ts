57ed9551a9d2f68adb0ef095ca53ca66f03d838c
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-02-14 09:23:51 -0300

    Add default tooltip component
    Adapt README to consider not using a custom Tooltip component the default behavior.

---
+++ src/utils/defaultConfig.ts
@@ -0,0 +1,8 @@
+import { PluginConfig } from '../types'
+import DefaultTooltip from '../components/DefaultTooltip.vue'
+
+const defaultPluginConfig: PluginConfig = {
+    component: DefaultTooltip,
+}
+
+export default defaultPluginConfig
