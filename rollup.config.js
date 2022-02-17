c6df34d15491a40111fe7be1f587b30ccb593a81
Luke Roberts <luke@mewt.eu>
2022-02-17 15:28:52 +0000

    Add plugin to include required modules (uuid) in output

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'rollup'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript';
 import vuePlugin from 'rollup-plugin-vue2'
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default defineConfig({
   input: './src/index.ts',
@@ -11,11 +12,11 @@ export default defineConfig({
   },
   external: [
     'vue',
-    'uuid'
   ],
   plugins: [
     commonjs(),
     typescript(),
     vuePlugin(),
+    nodeResolve(),
   ]
 })
