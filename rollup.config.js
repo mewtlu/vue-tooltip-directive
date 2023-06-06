18279799ec2f6d880e3ee86b73da4a97191374a3
Luke Roberts <luke@mewt.eu>
2023-06-06 12:45:11 +0100

    Create separate CJS and ESM exports

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,48 @@
 import { defineConfig } from 'rollup'
+import packageConfig from './package.json'
 import commonjs from '@rollup/plugin-commonjs'
-import typescript from '@rollup/plugin-typescript';
+import typescript from '@rollup/plugin-typescript'
 import vuePlugin from 'rollup-plugin-vue2'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 
-export default defineConfig({
-  input: './src/index.ts',
-  output: {
-    dir: './dist',
-    format: 'commonjs'
-  },
-  external: [
-    'vue',
-    'uuid'
-  ],
-  plugins: [
-    commonjs(),
-    typescript(),
-    vuePlugin(),
-  ]
-})
+const sharedPlugins = [
+  vuePlugin(),
+  nodeResolve(),
+]
+
+export default [
+  defineConfig({
+    input: './src/index.ts',
+    output: {
+      dir: packageConfig.exports['.'].import,
+      format: 'esm',
+    },
+    external: [
+      'vue',
+    ],
+    plugins: [
+      commonjs(), // Needs to be before typescript plugin for some reason
+      typescript({
+        tsconfig: './tsconfig.json',
+      }),
+      ...sharedPlugins,
+    ]
+  }),
+  defineConfig({
+    input: './src/index.ts',
+    output: {
+      dir: packageConfig.exports['.'].require,
+      format: 'cjs',
+    },
+    external: [
+      'vue',
+    ],
+    plugins: [
+      commonjs(), // Needs to be before typescript plugin for some reason
+      typescript({
+        tsconfig: './tsconfig.cjs.json',
+      }),
+      ...sharedPlugins
+    ]
+  })
+]
